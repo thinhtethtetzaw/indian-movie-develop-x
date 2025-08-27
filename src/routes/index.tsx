fed77c806ed5fce1c162bceaf6914d9034dea900
Sa Aung Htet Nyein <saaunghtetnyein@Sas-Laptop.local>
2025-08-27 21:12:57 +0700

    redirection added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
+  beforeLoad: async () => {
+    return redirect({
+      to: "/home",
+    });
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello 234 "/"!</div>;
+  return <div>Hello from India Movie</div>;
 }
